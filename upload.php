f177a3970ef57dd5a2f97050f4c81f3d1072713b
mgaler <mgaler@catl0qlop00028.corp.cox.com>
2017-12-05 12:58:52 -0500

    added upload.php

---
+++ upload.php
@@ -0,0 +1,30 @@
+<?php
+   if(isset($_FILES['image'])){
+      $errors= array();
+      $file_name = $_FILES['image']['name'];
+      $file_size =$_FILES['image']['size'];
+      $file_tmp =$_FILES['image']['tmp_name'];
+      $file_type=$_FILES['image']['type'];
+      $file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
+      
+      
+      
+      
+      if(empty($errors)==true){
+         move_uploaded_file($file_tmp,"images/".$file_name);
+         echo "Success";
+      }else{
+         print_r($errors);
+      }
+   }
+?>
+<html>
+   <body>
+      
+      <form action="" method="POST" enctype="multipart/form-data">
+         <input type="file" name="image" />
+         <input type="submit"/>
+      </form>
+      
+   </body>
+</html>
